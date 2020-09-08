52a696654ade3c64a93978544f87c78b5e90063e
Jarrod Parkes <parkesfjarrod@gmail.com>
2020-09-08 13:22:56 -0500

    add additional edge inset constructor for equal horizontal and vertical

--- Habanero/Classes/Extensions/UIEdgeInsets+Helpers.swift
+++ Habanero/Classes/Extensions/UIEdgeInsets+Helpers.swift
@@ -7,15 +7,23 @@ extension UIEdgeInsets {
         self.init(top: inset, left: inset, bottom: inset, right: inset)
     }
 
-    /// Initializes `UIEdgeInsets` with equal insets for left and right, and zero for top and bottom.
-    /// - Parameter inset: The amount of inset to apply horizontally.
-    public init(horizontal inset: CGFloat) {
-        self.init(top: 0, left: inset, bottom: 0, right: inset)
+    /// Initializes `UIEdgeInsets` with equal insets for top-and-bottom and left-and-right.
+    /// - Parameters:
+    ///   - vertical: The amount of inset to apply vertically.
+    ///   - horizontal: The amount of inset to apply horizontally.
+    public init(vertical: CGFloat, horizontal: CGFloat) {
+        self.init(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
     }
 
-    /// Initializes `UIEdgeInsets` with equal insets for top and bottom, and zero for left and right.
+    /// Initializes `UIEdgeInsets` with equal insets for left-and-right, and zero for top-and-bottom.
+    /// - Parameter inset: The amount of inset to apply horizontally.
+    public init(horizontal inset: CGFloat) {
+        self.init(vertical: 0, horizontal: inset)
+    }
+
+    /// Initializes `UIEdgeInsets` with equal insets for top-and-bottom, and zero for left-and-right.
     /// - Parameter inset: The amount of inset to apply vertically.
     public init(vertical inset: CGFloat) {
-        self.init(top: inset, left: 0, bottom: inset, right: 0)
+        self.init(vertical: inset, horizontal: 0)
     }
 }
