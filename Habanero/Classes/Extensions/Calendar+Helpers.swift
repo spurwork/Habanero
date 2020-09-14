eb2198aa2ccf99fd3eefd72cc0bf7981a3dd3c19
Jarrod Parkes <parkesfjarrod@gmail.com>
2020-09-14 12:17:37 -0500

    make calendar helpers public

--- Habanero/Classes/Extensions/Calendar+Helpers.swift
+++ Habanero/Classes/Extensions/Calendar+Helpers.swift
@@ -1,12 +1,20 @@
 // MARK: - Calendar (Helpers)
 
 extension Calendar {
-    func trimTimeFromDate(_ fromDate: Date) -> Date? {
+    /// Removes all time components from a `Date` so that only year, month, and day information is retained.
+    /// - Parameter fromDate: The date to remove time components from.
+    /// - Returns: A `Date` with time components removed.
+    public func trimTimeFromDate(_ fromDate: Date) -> Date? {
         let components = dateComponents([.year, .month, .day], from: fromDate)
         return date(from: components)
     }
 
-    func dates(from fromDate: Date, to toDate: Date) -> [Date] {
+    /// Returns an array of `Date` objects from [`fromDate`, `toDate`].
+    /// - Parameters:
+    ///   - fromDate: The starting date.
+    ///   - toDate: The ending date.
+    /// - Returns: An array of `Date` objects.
+    public func dates(from fromDate: Date, to toDate: Date) -> [Date] {
         var dates: [Date] = []
 
         var date = fromDate
